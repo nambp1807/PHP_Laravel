98f55b82f029d98a16ce8e9628411d816cf5c144
Admin <admin@g.com>
2020-02-28 16:48:02 +0700

    up date Procfile

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -8,14 +8,35 @@ class WebController extends Controller
 {
 
     public function home(){
-        $product = [
+        $products = [
            [
-               'name'=>"",
-               'image'=>"",
+               'name'=>"Áo Luci",
+               'image'=>"img/cart-page/product-1.jpg",
                'price'=>"10$"
            ],
+            [
+                'name'=>"Áo Luci",
+                'image'=>"img/cart-page/product-2.jpg",
+                'price'=>"10$"
+            ],
+            [
+                'name'=>"Áo Luci",
+                'image'=>"img/cart-page/product-3.jpg",
+                'price'=>"10$"
+            ],
+            [
+                'name'=>"Áo Luci",
+                'image'=>"img/cart-page/product-1.jpg",
+                'price'=>"10$"
+            ],
+            [
+                'name'=>"Áo Luci",
+                'image'=>"img/cart-page/product-3.jpg",
+                'price'=>"10$"
+            ],
+
         ];
-        return view("home");
+        return view("home",['products'=>$products]);
     }
 
     public function product(){
