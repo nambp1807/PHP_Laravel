d213248b50d8d1d7c5c3c9cfe5adaac6df6f53da
Admin <admin@g.com>
2020-03-06 22:27:48 +0700

    up date 6/3

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -4,19 +4,19 @@
     <meta name="keywords" content="Fashi, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fashi | Template</title>
+    <title>@yield("title")</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/font-awesome.min.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/them-ify-icons.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/elegant-icons.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/owl.carousel.min.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/nice-select.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/jquery-ui.min.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/slick-nav.min.css")}}" type="text/css">
-    <link rel="stylesheet" href="{{asset("css/style.css")}}" type="text/css">
+    <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/font-awesome.min.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/them-ify-icons.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/elegant-icons.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/owl.carousel.min.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/nice-select.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/jquery-ui.min.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/slick-nav.min.css")}}" type="{{asset("text/css")}}">
+    <link rel="stylesheet" href="{{asset("css/style.css")}}" type="{{asset("text/css")}}">
 </head>
