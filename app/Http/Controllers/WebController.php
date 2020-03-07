2d0d76131fd19aeef48ac50fda7dd283a996e7a2
Admin <admin@g.com>
2020-03-07 23:43:10 +0700

    up date 6/3

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -22,12 +22,12 @@ class WebController extends Controller
         return view("listing",['product'=>$products]);
     }
 
-    public function product()//nó request bắt được cái id nhé
+    public function product()
     {
-        $product = Product::take(1)->get();//tra ve 1 object Product theo id
-//        $category_products = Product::Where('category_id',$product->category_id)->Where('id','!=',$product->id)->take(10)->get();
-//        $brand_products = Product::Where('brand_id',$product->brand_id)->Where('id','!=',$product->id)->take(10)->get();
-        return view('product',['product'=>$product]);
+        $product = Product::find(1);//tra ve 1 object Product theo id
+        $category_products = Product::Where('category_id',$product->category_id)->Where('id','!=',$product->id)->take(10)->get();
+        $brand_products = Product::Where('brand_id',$product->brand_id)->Where('id','!=',$product->id)->take(10)->get();
+        return view('product',['product'=>$product,'category_product'=>$category_products]);
     }
 
     public function cart()
